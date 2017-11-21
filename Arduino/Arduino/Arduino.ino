808cf376e07aca5206d34ba5fedd80ed5e00cb0a
Bryan Quatremare <tiplouf76320@live.fr>
2017-11-21 12:37:12 +0100

    Added function to find the dewpoint and send it on serial bus

--- Arduino/Arduino/Arduino.ino
+++ Arduino/Arduino/Arduino.ino
@@ -15,41 +15,54 @@ void setup()
 
 void loop()
 {
-    
+    float hum = 0;
+    float temp = 0;
+    float poro = 0;
+    if(Serial.available())
+    {
+        cons = consigne();
+    }
+    sensor(&hum, &temp);
+    poro = magnus(temp, hum);
+    Serial.println(cons);
+    Serial.println(poro);
+    Serial.println(temp);
+    Serial.println("");
+    delay(2000);
+      
+}
+
+float magnus(float temp, float hum)
+{
+    return (237.7 * ( 17.27 * temp / (237.7 + temp) + log(hum/100)) / (17.27 - (17.27 * temp / (237.7 + temp) + log(hum/100))));
 }
 
 int consigne()
 {
     char buffer[] = {' ',' ',' '};
-    while(!Serial.available());
     Serial.readBytesUntil('n', buffer, 3);
-    int cons = atoi(buffer);
-    // Debug
-    //Serial.println(cons);
-
+    cons = atoi(buffer);
+    /*Debug
+    Serial.println(cons);*/
+    
     return cons;
 }
 
-float sensor(float hum, float temp)
+void sensor(float *hum, float *temp)
 {
-    hum = dht.readHumidity(); // reading the humidity from the sensor
-    temp = dht.readTemperature(); // reading the temperature from the sensor
+    *hum = dht.readHumidity(); // reading the humidity from the sensor
+    *temp = dht.readTemperature(); // reading the temperature from the sensor
 
-    if (isnan(hum) || isnan(temp)) // checking if read failed
+    if (isnan(*hum) || isnan(*temp)) // checking if read failed
     {
-      Serial.println("Failed to read from DHT sensor!!!");
+      Serial.println("Impossible de se connecter au capteur DHT22!!!");
       return;
     }
-
-      // Debug
+      /* Debug
       Serial.print("Humidité: ");
       Serial.println(hum);
       //Serial.println("%");
-      // Debug
       Serial.print("Température: ");
       Serial.println(temp);
-      //Serial.println("*C");
-      Serial.println("");
-
-      return hum, temp;
+      Serial.println("");*/
 }
