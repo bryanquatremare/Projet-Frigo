589c559e3499075b649dd7248af97cadb7e564cd
Bryan Quatremare <tiplouf76320@live.fr>
2017-11-21 10:05:58 +0100

    Added the function to recover setpoint

--- Arduino/Arduino/Arduino.ino
+++ Arduino/Arduino/Arduino.ino
@@ -1,35 +1,55 @@
-#include "DHT.h"
+/*#include "DHT.h"
 
 #define PIN 2
 
 DHT dht(PIN, DHT22); // initializing DHT sensor
 
+// initializing setpoint as global variable because we have to initialise it at 0 but not have to reset at every loop
+int cons = 0;*/
+
 void setup()
 {
     Serial.begin(9600);
-    dht.begin();
+    //dht.begin();
 }
 
 void loop()
 {
-    float hum = dht.readHumidity(); // reading the humidity from the sensor
-    float temp = dht.readTemperature(); // reading the temperature from the sensor
+  
+}
 
-    if(isnan(hum) || isnan(temp)) // checking if read failed
+int send()
+{
+    char buffer[] = {' ',' ',' ',' '};
+    while(!Serial.available());
+    Serial.readBytesUntil('n', buffer, 4);
+    int cons = atoi(buffer);
+    // Debug
+    //Serial.println(cons);
+
+    return cons;
+}
+
+float sensor(float hum, float temp)
+{
+    hum = dht.readHumidity(); // reading the humidity from the sensor
+    temp = dht.readTemperature(); // reading the temperature from the sensor
+
+    if (isnan(hum) || isnan(temp)) // checking if read failed
     {
-        Serial.println("Failed to read from DHT sensor!!!");
-        return;
+      Serial.println("Failed to read from DHT sensor!!!");
+      return;
     }
 
-    /* Debug
-    Serial.print("Humidité: ");*/
-    Serial.println(hum);
-    //Serial.println("%");
-    /* Debug
-    Serial.print("Température: ");*/
-    Serial.println(temp);
-    //Serial.println("*C");
-    Serial.println("");
-    
-    delay(2000);
+      // Debug
+      Serial.print("Humidité: ");
+      Serial.println(hum);
+      //Serial.println("%");
+      // Debug
+      Serial.print("Température: ");
+      Serial.println(temp);
+      //Serial.println("*C");
+      Serial.println("");
+
+      return hum, temp;
 }
