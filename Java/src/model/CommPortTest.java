9942f79af9a88e9b87e28dc4a55d0a8c7ff34e2e
Marand Virgile <marandlycee@gmail.com>
2017-11-21 09:25:20 +0100

    Classes de base

---
+++ Java/src/model/CommPortTest.java
@@ -0,0 +1,30 @@
+package model;
+import gnu.io.*;
+import java.util.Enumeration;
+
+public class CommPortTest {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Démarrage de la pénétration vers l'Arduino");
+		
+		CommPortIdentifier serialPort;
+		
+		Enumeration enumComm;
+		
+		enumComm = CommPortIdentifier.getPortIdentifiers();
+		
+		while(enumComm.hasMoreElements())
+		{
+			
+			serialPort=(CommPortIdentifier)enumComm.nextElement();
+			
+			if(serialPort.getPortType() == CommPortIdentifier.PORT_SERIAL)
+			{
+				System.out.println("L'Arduino est sur le port " + serialPort.getName());
+			}
+		}
+		
+		System.out.println("Fin de la réucpération des données");
+	}
+}
