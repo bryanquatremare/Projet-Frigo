888c9fcac57932f34cfa7c7778a43459ebb980ef
Unknown <corentin.dragee@viacesi.fr>
2017-11-21 15:54:21 +0100

    added peltier module in Arduino

--- Arduino/Arduino/Arduino.ino
+++ Arduino/Arduino/Arduino.ino
@@ -6,11 +6,15 @@ DHT dht(PIN, DHT22); // initializing DHT sensor
 
 // initializing setpoint as global variable because we have to initialise it at 0 but not have to reset at every loop
 int cons = 25;
+int peltier =3; //digital pin 3
+int power =0; //power level 0 to 99%
+int peltier_level = map(power, 0, 99, 0, 255);//This is a value from 0 to 255 that actually controls the MOSFET
 
 void setup()
 {
     Serial.begin(9600);
     dht.begin();
+    //pinMode(peltier, OUTPUT);
 }
 
 void loop()
@@ -19,6 +23,28 @@ void loop()
     float temp = 0;
     float poro = 0;
     boolean alerte = false;
+    char option;
+
+if(Serial.available() > 0)
+{
+option = Serial.read();
+if(option == 'a') 
+power += 5;
+else if(option == 'z')
+power -= 5;
+
+if(power > 99) power = 99;
+if(power < 0) power = 0;
+
+peltier_level = map(power, 0, 99, 0, 255);
+}
+
+Serial.print("Power=");
+Serial.print(power);
+Serial.print(" PLevel=");
+Serial.println(peltier_level);
+
+analogWrite(peltier, peltier_level); //Write this new value out to the port
     if(Serial.available())
     {
         cons = consigne();
