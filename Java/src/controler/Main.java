d19ed3c429c6230d3273fd7eaab979c5e03fbe0a
Nenrikido <ampy00@live.fr>
2017-11-21 10:12:57 +0100

    Updated main

--- Java/src/controler/Main.java
+++ Java/src/controler/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// schedule this for the event dispatch thread (edt)
 		MaFrame maframe = new MaFrame();
-		maframe.run(new SerialTest());
+		SerialTest serialTest = new SerialTest();
+		maframe.run(serialTest);
 	}
 }
